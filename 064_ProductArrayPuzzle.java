7579477b2f2ec0f9dd39f30252e793d884ac2c4d
keerthisureka <keerthisureka03@gmail.com>
2025-01-17 01:15:20 +0530

    64

--- 064_ProductArrayPuzzle.java
+++ 064_ProductArrayPuzzle.java
@@ -1,4 +1,4 @@
-// Approach: (2 iterations L to R and R to L -> Prefix and Suffix Product)
+// Approach-1: (2 iterations L to R and R to L -> Prefix and Suffix Product)
 // TC: O(n)
 // SC: O(1) -> apart from ans[] array
 // User function Template for Java
@@ -17,6 +17,48 @@ class Solution {
         for(int i = n - 2; i >= 0; i--) {
             prod *= arr[i + 1];
             ans[i] = ans[i] * prod;
+        }
+        return ans;
+    }
+}
+
+
+// Approach-2: (Calculate product of all non-zero numbers and handle cases based on count of zeros in array)
+// TC: O(n)
+// SC: O(1) -> apart from ans[] array
+class Solution {
+    public static int[] productExceptSelf(int[] arr) {
+        int n = arr.length;
+        int allProducts = 1;
+        int zeroCount = 0;
+        int zeroIdx = -1;
+
+        // Calculate the product of all non-zero elements and count zeros
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == 0) {
+                zeroCount++;
+                zeroIdx = i;
+            }
+            else {
+                allProducts *= arr[i];
+            }
+        }
+
+        int[] ans = new int[n]; // Initialize result array with zeros
+
+        if(zeroCount > 1) {
+            // If more than one zero, all products are zero
+            return ans;
+        }
+        else if(zeroCount == 1) {
+            // If exactly one zero, only that index gets the product
+            ans[zeroIdx] = allProducts;
+        }
+        else {
+            // If no zeros, calculate the result for all elements
+            for(int i = 0; i < n; i++) {
+                ans[i] = allProducts / arr[i];
+            }
         }
         return ans;
     }
