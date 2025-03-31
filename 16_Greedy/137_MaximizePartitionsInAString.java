d916260b56806b179144e52778ac43589d459f84
keerthisureka <keerthisureka03@gmail.com>
2025-03-31 12:47:07 +0530

    137

--- 16_Greedy/137_MaximizePartitionsInAString.java
+++ 16_Greedy/137_MaximizePartitionsInAString.java
@@ -35,7 +35,7 @@ class Solution {
     Dry Run
 
     Input:
-    s = "abacbc"
+    s = "acbbcc"
 
     Step 1: Compute Rightmost Occurrences
     ------------------------------------------------------
@@ -45,24 +45,24 @@ class Solution {
     Iteration 1: i = 5, s[i] = 'c'
         rightIdx['c' - 'a'] = 5
 
-    Iteration 2: i = 4, s[i] = 'b'
-        rightIdx['b' - 'a'] = 4
-
-    Iteration 3: i = 3, s[i] = 'c'
+    Iteration 2: i = 4, s[i] = 'c'
         (Already set, no change)
 
-    Iteration 4: i = 2, s[i] = 'a'
-        rightIdx['a' - 'a'] = 2
+    Iteration 3: i = 3, s[i] = 'b'
+        rightIdx['b' - 'a'] = 3
 
-    Iteration 5: i = 1, s[i] = 'b'
+    Iteration 4: i = 2, s[i] = 'b'
+        (Already set, no change)
+
+    Iteration 5: i = 1, s[i] = 'c'
         (Already set, no change)
 
     Iteration 6: i = 0, s[i] = 'a'
-        (Already set, no change)
+        rightIdx['a' - 'a'] = 0
 
     rightIdx after computation:
-        rightIdx['a'] = 2
-        rightIdx['b'] = 4
+        rightIdx['a'] = 0
+        rightIdx['b'] = 3
         rightIdx['c'] = 5
 
     Step 2: Partitioning the String
@@ -71,26 +71,29 @@ class Solution {
         i = 0, j = 0, partitions = 0
 
     Iteration 1: i = 0, s[i] = 'a'
-        j = max(0, rightIdx['a']) = max(0, 2) = 2
+        j = max(0, rightIdx['a']) = max(0, 0) = 0
+        Partition completed → partitions = 1
+        Move i to next index → i = 1
 
-    Iteration 2: i = 1, s[i] = 'b'
-        j = max(2, rightIdx['b']) = max(2, 4) = 4
+    Iteration 2: i = 1, s[i] = 'c'
+        j = max(1, rightIdx['c']) = max(1, 5) = 5
 
-    Iteration 3: i = 2, s[i] = 'a'
-        j = max(4, rightIdx['a']) = max(4, 2) = 4
+    Iteration 3: i = 2, s[i] = 'b'
+        j = max(5, rightIdx['b']) = max(5, 3) = 5
         (No change)
 
-    Iteration 4: i = 3, s[i] = 'c'
-        j = max(4, rightIdx['c']) = max(4, 5) = 5
+    Iteration 4: i = 3, s[i] = 'b'
+        j = max(5, rightIdx['b']) = max(5, 3) = 5
+        (No change)
 
-    Iteration 5: i = 4, s[i] = 'b'
-        j = max(5, rightIdx['b']) = max(5, 4) = 5
+    Iteration 5: i = 4, s[i] = 'c'
+        j = max(5, rightIdx['c']) = max(5, 5) = 5
         (No change)
 
     Iteration 6: i = 5, s[i] = 'c'
         j = max(5, rightIdx['c']) = max(5, 5) = 5
         (No change)
-        Partition completed → partitions = 1
+        Partition completed → partitions = 2
         Move i to next index → i = 6
 
     Step 3: Return number of partitions
